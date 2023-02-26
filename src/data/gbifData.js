ff01f09207ee145de88276e54787a5fc38c8a74c
Félicien Corbat <felicien@champis.net>
2023-02-26 03:31:14 +0100

    Remove deserialize

--- src/data/gbifData.js
+++ src/data/gbifData.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {parseAsSpecies} from "@/classes/Species.ts";
 
 const baseUrl = 'https://api.gbif.org/v1';
 async function getSpeciesList(search) {
@@ -14,10 +13,10 @@ async function getSpeciesList(search) {
 }
 
 async function getSpecies(id) {
+    let species;
     const response = await axios.get(baseUrl+'/species/' + id);
-    return response.data.map(
-        response => parseAsSpecies(response)
-    );
+    species = response.data;
+    return species;
 }
 
 export { getSpeciesList, getSpecies };
